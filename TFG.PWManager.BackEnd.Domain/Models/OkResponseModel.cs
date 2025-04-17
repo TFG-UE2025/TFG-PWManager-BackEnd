a748d6d55d81795281c707d3eca29e0b8deee830
Miguel Angel Carreño Puerta <95093376+miguelkrreno@users.noreply.github.com>
2025-04-17 17:58:27 +0200

    Agregados ejemplos Swagger para UserModel y paginación

---
+++ TFG.PWManager.BackEnd.Domain/Models/OkResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TFG.PWManager.BackEnd.Domain.Models
+{
+    public class OkResponseModel
+    {
+        public int Id { get; set; }
+
+        public string? Message { get; set; }
+    }
+}
---
+++ src/TFG.PWManager.BackEnd.WebAPI/Controllers/Swagger/UserModelExample.cs
@@ -0,0 +1,54 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+using System.Diagnostics.CodeAnalysis;
+using TFG.PWManager.BackEnd.Domain.Models;
+
+namespace TFG.PWManager.BackEnd.WebAPI.Controllers.Swagger
+{
+    [ExcludeFromCodeCoverage]
+    public class UserModelExample : IExamplesProvider<UserModel>
+    {
+        public UserModel GetExamples()
+        {
+            return new UserModel
+            {
+                Id = 1,
+                DisplayName = "Display Name",
+                Name = "User Name",
+                Surname = "User Surname",
+                PhoneNumber = "User Phone Number",
+                Email = "user@mail.com",
+                PasswordHash = "xxxXXXXXXXX==",
+                ActiveChk = true,
+                LanguageId = 1,
+                LanguageCode = "SP",
+                PasswordExpiredDate= DateTime.Now,
+            };
+        }
+    }
+
+    [ExcludeFromCodeCoverage]
+    public class UserModelListExample : IExamplesProvider<IEnumerable<UserModel>>
+    {
+        public IEnumerable<UserModel> GetExamples()
+        {
+            return new List<UserModel> {
+                new UserModelExample().GetExamples()
+            };
+        }
+    }
+
+    [ExcludeFromCodeCoverage]
+    public class UserPaginationModelExample : IExamplesProvider<DataPaginationModel<UserModel>>
+    {
+        public DataPaginationModel<UserModel> GetExamples()
+        {
+            return new DataPaginationModel<UserModel>()
+            {
+                PageNumber = 1,
+                PageSize = 10,
+                TotalSize = 100,
+                Data = new UserModelListExample().GetExamples()
+            };
+        }
+    }
+}
