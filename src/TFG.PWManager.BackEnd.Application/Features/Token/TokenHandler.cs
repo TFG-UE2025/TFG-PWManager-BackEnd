864dbd480d6c8a87d7622bb40094f815e95d8a5f
Miguel Angel Carreño Puerta <95093376+miguelkrreno@users.noreply.github.com>
2025-04-18 21:47:17 +0200

    implementaciond de handlers para usuario y token

---
+++ src/TFG.PWManager.BackEnd.Application/Features/Token/TokenHandler.cs
@@ -0,0 +1,59 @@
+﻿using MediatR;
+using TFG.PWManager.BackEnd.Application.Features.Token.Commans;
+using TFG.PWManager.BackEnd.Application.Features.Token.Queries;
+using TFG.PWManager.BackEnd.Domain.Commons;
+using TFG.PWManager.BackEnd.Domain.Models;
+
+namespace TFG.PWManager.BackEnd.Application.Features.Token
+{
+    public class TokenHandler :
+        IRequestHandler<GetTokenByEmailQuery, TokenModel>,
+        IRequestHandler<GetEnabledTokenQuery, bool>,
+        IRequestHandler<GetTokenRefreshByEmailQuery, TokenModel>,
+        IRequestHandler<GetTokenConfig, TokenConfigModel>,
+        IRequestHandler<AddTokenCommand, OkResponseModel>,
+        IRequestHandler<AddRefreshTokenCommand, OkResponseModel>,
+        IRequestHandler<PutTokenCommand, OkResponseModel>,
+        IRequestHandler<PutRefreshTokenCommand, OkResponseModel>
+    {
+        public Task<TokenModel> Handle(GetTokenByEmailQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Handle(GetEnabledTokenQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TokenModel> Handle(GetTokenRefreshByEmailQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TokenConfigModel> Handle(GetTokenConfig request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OkResponseModel> Handle(AddTokenCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OkResponseModel> Handle(AddRefreshTokenCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OkResponseModel> Handle(PutTokenCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OkResponseModel> Handle(PutRefreshTokenCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/TFG.PWManager.BackEnd.Application/Features/User/UserHandler.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using TFG.PWManager.BackEnd.Application.Features.User.Commands;
+using TFG.PWManager.BackEnd.Application.Features.User.Queries;
+using TFG.PWManager.BackEnd.Domain.Models;
+
+namespace TFG.PWManager.BackEnd.Application.Features.User
+{
+    public class UserHandler :
+       IRequestHandler<GetUserByEmailQuery, UserModel>,
+       IRequestHandler<CreateUserCommand, OkResponseModel>,
+       IRequestHandler<UpdateUserCommand, OkResponseModel>,
+       IRequestHandler<DeleteUserCommand, OkResponseModel>,
+       IRequestHandler<ChangeUserPasswordCommand, OkResponseModel>,
+       IRequestHandler<GetThreeLastsUserKeysByUserIdQuery, IEnumerable<UserKeyModel>>,
+       IRequestHandler<GetLastUserKeyByUserIdQuery, UserKeyModel>
+    {
+        public Task<UserModel> Handle(GetUserByEmailQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OkResponseModel> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OkResponseModel> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OkResponseModel> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OkResponseModel> Handle(ChangeUserPasswordCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UserKeyModel>> Handle(GetThreeLastsUserKeysByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserKeyModel> Handle(GetLastUserKeyByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
